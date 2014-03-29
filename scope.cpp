8efda2c2b0ea08d149a268e17144007adf7fb6b0
vt4a2h <vt4a2h@gmail.com>
2014-03-30 01:16:11 +0700

    added child scopes for each scope

--- scope.cpp
+++ scope.cpp
@@ -10,8 +10,9 @@
 
 namespace entity {
 
-    Scope::Scope(const QString &scopeName)
+    Scope::Scope(const QString &scopeName, Scope *parent)
         : m_Name(scopeName)
+        , m_ParentScope(parent)
     {
     }
 
@@ -40,6 +41,18 @@ namespace entity {
         return (m_Types.contains(name) ? m_Types[name] : nullptr);
     }
 
+    SharedType Scope::takeType(const QString &name)
+    {
+        SharedType result(nullptr);
+
+        if (m_Types.contains(name)) {
+            result = m_Types[name];
+            m_Types.remove(name);
+        }
+
+        return result;
+    }
+
     SharedType Scope::addType(const QString &name, UserType kindOfType)
     {
         switch (kindOfType) {
@@ -55,6 +68,16 @@ namespace entity {
                 return *m_Types.insert(name, std::make_shared<Enum>(name, this));
         }
         return nullptr;
+    }
+
+    void Scope::addType(SharedType type, bool &operationStatus)
+    {
+        operationStatus = false;
+        if (!m_Types.contains(type->name())) {
+            type->setScope(this);
+            m_Types.insert(type->name(), type);
+            operationStatus = true;
+        }
     }
 
     bool Scope::containsType(const QString &name) const
@@ -78,10 +101,32 @@ namespace entity {
         return (m_ExtendedType.contains(key) ? m_ExtendedType[key] : nullptr);
     }
 
+    SharedExtendedType Scope::takeExtendedType(const QString &alias)
+    {
+        SharedExtendedType result(nullptr);
+
+        if (m_ExtendedType.contains(alias)) {
+            result = m_ExtendedType[alias];
+            m_ExtendedType.remove(alias);
+        }
+
+        return result;
+    }
+
     SharedExtendedType Scope::addExtendedType(const QString &alias, SharedType type)
     {
         return *m_ExtendedType.insert(QString("%1-%2").arg(alias, type->name()),
                                       std::make_shared<ExtendedType>(this, type.get(), alias));
+    }
+
+    void Scope::addExtendedType(SharedExtendedType extendedType, bool &operationStatus)
+    {
+        operationStatus = false;
+        if (!m_ExtendedType.contains(extendedType->alias())) {
+            extendedType->setScope(this);
+            m_ExtendedType.insert(extendedType->alias(), extendedType);
+            operationStatus = true;
+        }
     }
 
     bool Scope::containsExtendedType(const QString &alias, SharedType type) const
@@ -97,6 +142,53 @@ namespace entity {
     ExtendedTypesList Scope::extendedTypes() const
     {
         return m_ExtendedType.values();
+    }
+
+    SharedScope Scope::getChildScope(const QString &name)
+    {
+        return (m_Scopes.contains(name) ? m_Scopes[name] : nullptr);
+    }
+
+    SharedScope Scope::takeChildScope(const QString &name)
+    {
+        SharedScope result(nullptr);
+
+        if (m_Scopes.contains(name)) {
+            result = m_Scopes[name];
+            m_Scopes.remove(name);
+        }
+
+        return result;
+    }
+
+    SharedScope Scope::addChildScope(const QString &name)
+    {
+        return *m_Scopes.insert(name, std::make_shared<Scope>(name, this));
+    }
+
+    void Scope::addChildScope(SharedScope scope, bool &operationStatus)
+    {
+        operationStatus = false;
+        if (!m_Scopes.contains(scope->name())) {
+            scope->setParentScope(this);
+            m_Scopes.insert(scope->name(), scope);
+            operationStatus = true;
+        }
+    }
+
+    bool Scope::containsChildScope(const QString &name)
+    {
+        return m_Scopes.contains(name);
+    }
+
+    void Scope::removeChildScope(const QString &name)
+    {
+        m_Scopes.remove(name);
+    }
+
+    ScopesList Scope::scopes() const
+    {
+        return m_Scopes.values();
     }
 
 } // namespace entity
--- scope.h
+++ scope.h
@@ -10,7 +10,7 @@ namespace entity {
     class Scope
     {
     public:
-        Scope(const QString &scopeName = "");
+        Scope(const QString &scopeName = "", Scope *parent = nullptr);
 
         QString name() const;
         void setName(const QString &name);
@@ -19,23 +19,36 @@ namespace entity {
         void setParentScope(Scope *parentScope);
 
         SharedType getType(const QString &name) const;
+        SharedType takeType(const QString &name);
         SharedType addType(const QString &name, UserType kindOfType);
+        void       addType(SharedType type, bool &operationStatus);
         bool containsType(const QString &name) const;
         void removeType(const QString &name);
         TypesList types() const;
 
         SharedExtendedType getExtendedType(const QString &alias, SharedType type) const;
+        SharedExtendedType takeExtendedType(const QString &alias);
         SharedExtendedType addExtendedType(const QString &alias, SharedType type);
+        void addExtendedType(SharedExtendedType extendedType, bool &operationStatus);
         bool containsExtendedType(const QString &alias, SharedType type) const;
         void removeExtendedType(const QString &alias, SharedType type);
         ExtendedTypesList extendedTypes() const;
+
+        SharedScope getChildScope(const QString &name);
+        SharedScope takeChildScope(const QString &name);
+        SharedScope addChildScope(const QString &name);
+        void        addChildScope(SharedScope scope, bool &operationStatus);
+        bool containsChildScope(const QString &name);
+        void removeChildScope(const QString &name);
+        ScopesList scopes() const;
 
     private:
         void swap(Scope &other);
 
         QString m_Name;
         Scope  *m_ParentScope;
-        Types m_Types;
+        Scopes m_Scopes;
+        Types  m_Types;
         ExtendedTypes m_ExtendedType;
     };
 
