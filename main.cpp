5b7a46534680b965f92cfaafb16ebdffd654b58e
vt4a2h <vt4a2h@gmail.com>
2014-04-13 00:27:21 +0700

    fix main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-#include "globaldatabase.h"
-
 int main()
 {
-    db::GlobalDatabase::instance();
-    db::GlobalDatabase::instance();
-    db::GlobalDatabase::instance();
-    db::GlobalDatabase::instance();
-    db::GlobalDatabase::instance();
-
     return 0;
 }
