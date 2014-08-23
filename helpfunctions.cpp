75d0afbf5c53fed97f30e0a1ff835af7112c7a2c
vt4a2h <vt4a2h@gmail.com>
2014-08-23 23:00:40 +0700

    improved helpers

--- helpfunctions.cpp
+++ helpfunctions.cpp
@@ -36,80 +36,75 @@ namespace utility {
         object.contains(key) ? func() : lst.append(QObject::tr("Key \"%1\" not found!").arg(key));
     }
 
-    // TODO: improve it
+    namespace {
+        using KeywordsString = const std::map<entity::FieldKeyword,     QString>;
+        using LhsIdString    = const std::map<entity::LhsIdentificator, QString>;
+        using RhsIdString    = const std::map<entity::RhsIdentificator, QString>;
+        using SectionString  = const std::map<entity::Section,          QString>;
+
+        KeywordsString kKeywords { {entity::Volatile, "volatile"}, {entity::Mutable, "mutable"},
+                                   {entity::FieldStatic, "static"} };
+
+        LhsIdString    kLhsId    { {entity::Explicit, "explicit"},   {entity::Inline, "inline"},
+                                   {entity::MethodStatic, "static"}, {entity::Virtual, "virtual"},
+                                   {entity::Friend, "friend"} };
+
+        RhsIdString    kRhsId    { {entity::None, ""}, {entity::Override, "override"}, {entity::Final, "final"},
+                                   {entity::Delete, "= delete"}, {entity::Default, "= default"},
+                                   {entity::PureVirtual, "= 0"} };
+
+        SectionString  kSection  { {entity::Public, "public"}, {entity::Protected, "protected"},
+                                   {entity::Private, "private"} };
+
+        using MakerT  = std::function<entity::SharedType()>;
+        using MakersT = const std::map<entity::UserType, MakerT>;
+        using MakerM  = std::function<entity::SharedMethod()>;
+        using MakersM = const std::map<entity::ClassMethodType, MakerM>;
+        using MakerR  = std::function<relationship::SharedRelation()>;
+        using MakersR = const std::map<relationship::RelationType, MakerR>;
+
+        MakersT kType = { {entity::BasicType,        [](){ return std::make_shared<entity::Type>();          }},
+                         {entity::UserClassType,     [](){ return std::make_shared<entity::Class>();         }},
+                         {entity::TemplateClassType, [](){ return std::make_shared<entity::TemplateClass>(); }},
+                         {entity::UnionType,         [](){ return std::make_shared<entity::Union>();         }},
+                         {entity::EnumType,          [](){ return std::make_shared<entity::Enum>();          }}
+                        };
+
+        MakersR kRelation = {
+            {relationship::AggregationRelation,    [](){ return std::make_shared<relationship::MultiplyAssociation>(); }},
+            {relationship::CompositionRelation,    [](){ return std::make_shared<relationship::MultiplyAssociation>(); }},
+            {relationship::MultiRelation,          [](){ return std::make_shared<relationship::MultiplyAssociation>(); }},
+            {relationship::AssociationRelation,    [](){ return std::make_shared<relationship::Association>();         }},
+            {relationship::DependencyRelation ,    [](){ return std::make_shared<relationship::Dependency>();          }},
+            {relationship::GeneralizationRelation, [](){ return std::make_shared<relationship::Generalization>();      }},
+            {relationship::RealizationRelation,    [](){ return std::make_shared<relationship::Realization>();         }},
+            {relationship::SimpleRelation,         [](){ return std::make_shared<relationship::Relation>();            }}
+        };
+
+        MakersM kMethod = { {entity::SimpleMethod,   [](){ return std::make_shared<entity::ClassMethod>();         }},
+                            {entity::TemplateMethod, [](){ return std::make_shared<entity::TemplateClassMethod>(); }}
+                          };
+    }
 
     std::shared_ptr<entity::Type> makeType(entity::UserType type)
     {
-        using namespace entity;
-        switch (type) {
-            case BasicType:
-                return std::make_shared<Type>();
-            case UserClassType:
-                return std::make_shared<Class>();
-            case TemplateClassType:
-                return std::make_shared<TemplateClass>();
-            case UnionType:
-                return std::make_shared<Union>();
-            case EnumType:
-                return std::make_shared<Enum>();
-            default:
-                return std::make_shared<Type>();
-        }
+        MakerT defaultMaker([](){ return std::make_shared<entity::Type>(); });
+
+        return mapSearchHelper(kType, type, defaultMaker)();
     }
 
     std::shared_ptr<relationship::Relation> makeRelation(relationship::RelationType relation)
     {
-        using namespace relationship;
-        switch (relation) {
-            case AggregationRelation:
-            case CompositionRelation:
-            case MultiRelation:
-                return std::make_shared<MultiplyAssociation>();
-            case AssociationRelation:
-                return std::make_shared<Association>();
-            case DependencyRelation:
-                return std::make_shared<Dependency>();
-            case GeneralizationRelation:
-                return std::make_shared<Generalization>();
-            case RealizationRelation:
-                return std::make_shared<Realization>();
-            case SimpleRelation:
-                return std::make_shared<Relation>();
-            default:
-                return std::make_shared<Relation>();
-        }
+        MakerR defaultMaker([](){ return std::make_shared<relationship::Relation>(); });
+
+        return mapSearchHelper(kRelation, relation, defaultMaker)();
     }
 
     std::shared_ptr<entity::ClassMethod> makeMethod(entity::ClassMethodType methodType)
     {
-        using namespace entity;
-        switch (methodType) {
-            case SimpleMethod:
-                return std::make_shared<ClassMethod>();
-            case TemplateMethod:
-                return std::make_shared<TemplateClassMethod>();
-            default:
-                return std::make_shared<ClassMethod>();
-        }
-    }
+        MakerM defaultMaker([](){ return std::make_shared<entity::ClassMethod>(); });
 
-    namespace {
-        using namespace entity;
-
-        using KeywordsString = const std::map<FieldKeyword,     QString>;
-        using LhsIdString    = const std::map<LhsIdentificator, QString>;
-        using RhsIdString    = const std::map<RhsIdentificator, QString>;
-        using SectionString  = const std::map<Section,          QString>;
-
-        KeywordsString kKeywords { {Volatile, "volatile"}, {Mutable, "mutable"}, {FieldStatic, "static"} };
-
-        LhsIdString    kLhsId    { {Explicit, "explicit"}, {Inline, "inline"},   {MethodStatic, "static"},
-                                   {Virtual, "virtual"},   {Friend, "friend"} };
-
-        RhsIdString    kRhsId    { {None, ""}, {Override, "override"}, {Final, "final"}, {Delete, "= delete"},
-                                   {Default, "= default"}, {PureVirtual, "= 0"} };
-
-        SectionString  kSection  { {Public, "public"}, {Protected, "protected"}, {Private, "private"} };
+        return mapSearchHelper(kMethod, methodType, defaultMaker)();
     }
 
     QString fieldKeywordToString(entity::FieldKeyword keyword)
