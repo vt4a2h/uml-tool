2506d472ba621a39148bcc389864a2e4d88e42c4
vt4a2h <vt4a2h@gmail.com>
2014-10-10 15:07:23 +0700

    added test data

---
+++ tests/test_data/shared_pointer.h
@@ -0,0 +1,48 @@
+namespace memory
+{
+    template<class Value>
+    class shared_pointer
+    {
+    public:
+        explicit shared_pointer(Value *value = nullptr);
+        ~shared_pointer();
+        void reset(Value *other = nullptr);
+        size_t use_count() const;
+        const Value *get() const;
+        Value *get();
+
+    private:
+        void swap(shared_pointer &src);
+    };
+
+    template<class Value>
+    shared_pointer<Value>::shared_pointer(Value *value)
+    {
+    }
+
+    template<class Value>
+    shared_pointer<Value>::~shared_pointer()
+    {
+    }
+
+    template<class Value>
+    void shared_pointer<Value>::reset(Value *other)
+    {
+    }
+
+    template<class Value>
+    size_t shared_pointer<Value>::use_count() const
+    {
+    }
+
+    template<class Value>
+    const Value *shared_pointer<Value>::get() const
+    {
+    }
+
+    template<class Value>
+    Value *shared_pointer<Value>::get()
+    {
+    }
+
+}
