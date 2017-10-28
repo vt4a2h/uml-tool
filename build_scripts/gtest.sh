4fb67faa37c94ff36e9cc0daf8686b2d088fd998
vt4a2h <vt4a2h@gmail.com>
2017-10-28 21:14:35 +0300

    Updated compiler version for gtest.

--- build_scripts/gtest.sh
+++ build_scripts/gtest.sh
@@ -4,9 +4,9 @@ git clone https://github.com/google/googletest.git
 
 rm -rf build && mkdir build && cd build
 
-CXX=g++-6
+CXX=g++-7
 CXX_FLAGS="-std=c++1z -Ofast"
-C=gcc-6
+C=gcc-7
 C_FLAGS="-std=c1x -Ofast"
 
 cmake ../googletest/ -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$C -DCMAKE_C_FLAGS=$C_FLAGS -DCMAKE_CXX_FLAGS=$CXX_FLAGS
