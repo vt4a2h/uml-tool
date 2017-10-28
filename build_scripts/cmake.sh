703054573a47b878d8ee6a235cea2b46852ad80a
vt4a2h <vt4a2h@gmail.com>
2017-10-29 00:20:44 +0300

    Reduced verbosity level when unpacking cmake for building.

--- build_scripts/cmake.sh
+++ build_scripts/cmake.sh
@@ -3,7 +3,7 @@
 rm -rf build && mkdir build && cd build
 
 wget https://cmake.org/files/v3.9/cmake-3.9.4.tar.gz
-tar -xzvf cmake-3.9.4.tar.gz
+tar -xzf cmake-3.9.4.tar.gz
 
 cd cmake-3.9.4/
 CXX=g++-7 ./bootstrap --parallel=4 &> /dev/null
